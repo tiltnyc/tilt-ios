9ccbcc229e06051babbb7c72d5f0815ff7358fa2
Adam Parrish <aparrish@neosavvy.com>
2012-02-11 00:21:24 -0500

    Fixing the issue with not dividing the percentages by 100

--- tilt-ios/tilt-ios/TiltTeamInvestmentsViewController.m
+++ tilt-ios/tilt-ios/TiltTeamInvestmentsViewController.m
@@ -70,7 +70,11 @@
         
         TiltTeamInvestment *investment = [[TiltTeamInvestment alloc] init];
         investment.team = (NSNumber *)[team.identifier copy];
-        investment.percentage = (NSNumber *)[team.percentInvested copy];
+        
+        double percentage = [(NSNumber *)[team.percentInvested copy] doubleValue];
+        double valueAfterConversion = percentage / 100;
+        
+        investment.percentage = [NSNumber numberWithDouble:valueAfterConversion];
         
         [teamInvestments addObject:investment];
     }
